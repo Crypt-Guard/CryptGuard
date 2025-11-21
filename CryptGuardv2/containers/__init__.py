25cfbeec87a9d5082e480b32b36ec3ef3d849f8f
CryptGuard <cryptguard737@gmail.com>
2025-11-21 13:52:24 -0300

    Create __init__.py

---
+++ CryptGuardv2/containers/__init__.py
@@ -0,0 +1,21 @@
+"""
+Módulo de Secure Containers para compartilhamento seguro.
+
+Este módulo implementa o formato de container .vault que permite
+transportar de forma segura itens do CryptGuard e KeyGuard.
+"""
+
+from .secure_container import (
+    ContainerEntry,
+    SecureContainerReader,
+    SecureContainerWriter,
+)
+from .storage_atomic import acquire_lock, atomic_save
+
+__all__ = [
+    "SecureContainerWriter",
+    "SecureContainerReader",
+    "ContainerEntry",
+    "acquire_lock",
+    "atomic_save",
+]
